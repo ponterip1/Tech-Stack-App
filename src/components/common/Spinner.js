5b4faebb3f4f247f2a2eea778d1b82dab3819d9c
ponterip1 <peterponterio@Peters-MacBook-Pro.local>
2017-07-16 23:11:41 -0600

    First commit

---
+++ src/components/common/Spinner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, ActivityIndicator } from 'react-native';
+
+//Spinner waiting logo
+const Spinner = ({ spinnerSize }) => {
+  return (
+    <View style={styles.spinnerStyle}>
+      <ActivityIndicator size={spinnerSize || 'large'} />
+    </View>
+  );
+};
+
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
+
+export { Spinner };
